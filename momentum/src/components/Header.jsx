f7f890d5ec273c8087482fe39ea2e8f47c2f37e3
danggeuni <102232204+danggeuni@users.noreply.github.com>
2023-01-26 22:16:47 +0900

    header update

--- momentum/src/components/Header.jsx
+++ momentum/src/components/Header.jsx
@@ -1,19 +1,27 @@
+import cx from "clsx";
+
 import { useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
   const [search, setSearch] = useState("");
+  const [active, setActive] = useState(true);
+
+  function activeToggle() {
+    setActive(!active);
+  }
 
   return (
     <div className={styles.header}>
       <div className={styles.search}>
-        <button onClick={() => {}} className={styles.toggle}>
+        {/* Search 버튼 클릭 시 input 창이 나타나게 만든다. */}
+        <button onClick={() => activeToggle()} className={styles.toggle}>
           Search
         </button>
         <div className={styles.inputWrapper}>
           <form className={styles.form}>
             <input
-              className={styles.input}
+              className={cx(styles.input, { [styles.active]: active })}
               onChange={(e) => setSearch(e.target.value)}
               value={search}
             ></input>
