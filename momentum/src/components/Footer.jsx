0b42b2d515180a8cff2fe0568fa59a5258eed04a
danggeuni <dydrnxkeh@naver.com>
2023-02-01 16:57:26 +0900

    onCreate

--- momentum/src/components/Footer.jsx
+++ momentum/src/components/Footer.jsx
@@ -1,18 +1,43 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState, useRef } from "react";
 import styles from "./Footer.module.css";
 import cx from "clsx";
 
+// reducer CRUD 함수
+const reducer = (state, action) => {
+  let newState = [];
+
+  switch (action.type) {
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+
+    case "REMOVE": {
+      newState = state.filter((item) => item.id !== action.targetId);
+      break;
+    }
+
+    case "EDIT": {
+      newState = state.map((item) =>
+        item.id === action.targetId ? { ...action.data } : item
+      );
+      break;
+    }
+
+    default:
+      return state;
+  }
+  localStorage.setItem("list", JSON.stringify(newState));
+  return newState;
+};
+
 export default function Footer() {
-  const [data, setData] = useState({ content: "", author: "" });
+  const [text, setData] = useState({ content: "", author: "" });
   const [toggleOn, setToggleon] = useState(false);
-
-  // input의 값 state 만들고, form으로 submit시 local에 넘어가야함.
-  // form의 submit의 state는 배열로 진행하고, setState([newData,...state]) 형식으로 가야함
-  // local에 data가 존재할 경우, 기존 p태그를 숨기고 ul > map을 통해 li화 시켜야함.
-  // 리스트는 별도의 컴포넌트로 만들어 수정버튼과 삭제버튼을 위치 시켜야함.
-
-  const [todoText, setTodoText] = useState([]);
+  const [todoText, setTodoText] = useState("");
+  const dataId = useRef(0);
+  const [data, dispatch] = useReducer(reducer, []);
 
   useEffect(() => {
     async function fetch() {
@@ -24,13 +49,33 @@ export default function Footer() {
     fetch();
   }, []);
 
+  //
+
+  // input의 값 state 만들고, form으로 submit시 local에 넘어가야함.
+  // form의 submit의 state는 배열로 진행하고, setState([newData,...state]) 형식으로 가야함
+  // local에 data가 존재할 경우, 기존 p태그를 숨기고 ul > map을 통해 li화 시켜야함.
+  // 리스트는 별도의 컴포넌트로 만들어 수정버튼과 삭제버튼을 위치 시켜야함.
+
   function todoClick() {
     setToggleon(!toggleOn);
   }
 
+  // 아이템 생성 함수
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      state: {
+        id: dataId.current,
+        content,
+      },
+    });
+    dataId.current += 1;
+  };
+
   // 이벤트를 막고 input value를 로컬로 보내자.
   function todoSubmit(e) {
     e.preventDefault();
+    onCreate(todoText);
   }
 
   return (
@@ -39,8 +84,8 @@ export default function Footer() {
         <div className={styles.description}></div>
 
         <div className={styles.quote}>
-          <p className={styles.content}>{data.content}</p>
-          <cite className={styles.author}>{data.author}</cite>
+          <p className={styles.content}>{text.content}</p>
+          <cite className={styles.author}>{text.author}</cite>
         </div>
 
         <button onClick={todoClick} className={styles.toggle}>
